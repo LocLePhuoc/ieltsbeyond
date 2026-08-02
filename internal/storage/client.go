b47687c85ae0a237112c12f3fd6d2e44917b6ecb
loclp <loclp@vng.com.vn>
2026-08-02 19:44:29 +0700

    Storage: add upload funcs

--- internal/storage/client.go
+++ internal/storage/client.go
@@ -1,19 +1,26 @@
 package storage
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"os"
 	"time"
+
+	logger "ieltsbeyond/internal/logging"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awsconfig "github.com/aws/aws-sdk-go-v2/config"
 	awscredentials "github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/transfermanager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/smithy-go"
 )
 
 var Instance, _ = NewClient()
 
 type Client struct {
+	client  *s3.Client
 	presign *s3.PresignClient
 }
 
@@ -39,7 +46,7 @@ func NewClient() (*Client, error) {
 		}
 	})
 
-	return &Client{presign: s3.NewPresignClient(client)}, nil
+	return &Client{client: client, presign: s3.NewPresignClient(client)}, nil
 }
 
 func (s *Client) GetObjectURL(ctx context.Context, bucket string, objectKey string) (string, error) {
@@ -52,4 +59,66 @@ func (s *Client) GetObjectURL(ctx context.Context, bucket string, objectKey stri
 		return "", err
 	}
 	return req.URL, nil
+}
+
+func (s *Client) UploadObject(ctx context.Context, bucket string, objectKey string, filePath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		logger.Instance.Errorw("Couldn't open file to upload.",
+			"file", filePath,
+			"error", err,
+		)
+		return err
+	}
+	defer file.Close()
+	_, err = s.client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(objectKey),
+		Body:   file,
+	})
+
+	if err != nil {
+		var apiErr smithy.APIError
+		if errors.As(err, &apiErr) && apiErr.ErrorCode() == "EntityTooLarge" {
+			logger.Instance.Errorw("Object too large to upload. Needs multipart upload.",
+				"file", filePath,
+				"error", err,
+			)
+		} else {
+			logger.Instance.Errorw("Failed to upload object.", "file", filePath, "error", err)
+		}
+		return err
+	}
+
+	waiter := s3.NewObjectExistsWaiter(s.client)
+	err = waiter.Wait(ctx, &s3.HeadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(objectKey),
+	}, 2*time.Minute)
+	if err != nil {
+		logger.Instance.Errorw("Failed to wait for file to exist.", "objectKey", objectKey)
+	}
+	return err
+}
+
+func (s *Client) UploadLargeObject(ctx context.Context, bucket string, objectKey string, largeObject []byte) error {
+	largeBuffer := bytes.NewBuffer(largeObject)
+	var partMiBs int64 = 10
+
+	uploader := transfermanager.New(s.client, func(o *transfermanager.Options) {
+		o.PartSizeBytes = partMiBs * 1024 * 1024
+	})
+
+	_, err := uploader.UploadObject(ctx, &transfermanager.UploadObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(objectKey),
+		Body:   largeBuffer,
+	})
+	if err != nil {
+		logger.Instance.Errorw("Failed to upload large object.",
+			"objectKey", objectKey,
+			"error", err,
+		)
+	}
+	return err
 }
